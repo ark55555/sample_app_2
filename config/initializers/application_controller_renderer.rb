a98b19743a01aa6b49b60cfede0d6f9d2452deb3
ark55555 <ark.tooyoung@gmail.com>
2021-04-25 10:25:49 +0000

    [Add]5-2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b9380877392be334f98d425aba13d497f693b2366789ebd3be17bab329e0ed5b8dfdfdba2744f167586c161d706714c42179b6e0beb361c2276b9e5e43eaa58f'
